276e45e2e1dc89daf0e8589c3613ecc7aff321a9
Tomoyuki Kawaguchi <u0953876.karins.soft@icloud.com>
2021-06-22 13:37:28 +0900

    Angularフロントエンド側作成

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,17 +1,50 @@
 export const products = [
   {
+    coverImage: './assets/img/phone-cover.jpg',
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    heading1: 'A large phone with 4 color',
+    heading2: 'サンプルテキスト２',
+    heading3: 'サンプルテキスト３',
+    headingtext1: '',
+    headingtext2: '',
+    headingtext3: ''
   },
   {
+    coverImage: './assets/img/phone-cover.jpg',
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    heading1: 'A great phone with 6 color',
+    heading2: 'サンプルテキスト２',
+    heading3: 'サンプルテキスト３',
+    headingtext1: '詳細説明　詳細説明　詳細説明　詳細説明　詳細説明　詳細説明　詳細説明　詳細説明',
+    headingtext2: 'スペック　スペック　スペック　スペック　スペック　スペック　スペック　スペック',
+    headingtext3: '取扱方法　取扱方法　取扱方法　取扱方法　取扱方法　取扱方法　取扱方法　取扱方法　'
   },
   {
+    coverImage: './assets/img/phone-cover.jpg',
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: '',
+    heading1: 'A standard phone with 3 color',
+    heading2: 'サンプルテキスト２',
+    heading3: 'サンプルテキスト３',
+    headingtext1: '詳細説明　詳細説明　詳細説明　詳細説明　詳細説明　詳細説明　詳細説明　詳細説明',
+    headingtext2: 'スペック　スペック　スペック　スペック　スペック　スペック　スペック　スペック',
+    headingtext3: '取扱方法　取扱方法　取扱方法　取扱方法　取扱方法　取扱方法　取扱方法　取扱方法　'
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone Special',
+    price: 999,
+    description: '',
+    heading1: 'A special phone with 5 color',
+    heading2: 'サンプルテキスト２',
+    heading3: 'サンプルテキスト３',
+    headingtext1: '詳細説明　詳細説明　詳細説明　詳細説明　詳細説明　詳細説明　詳細説明　詳細説明',
+    headingtext2: 'スペック　スペック　スペック　スペック　スペック　スペック　スペック　スペック',
+    headingtext3: '取扱方法　取扱方法　取扱方法　取扱方法　取扱方法　取扱方法　取扱方法　取扱方法　'
   }
 ];
