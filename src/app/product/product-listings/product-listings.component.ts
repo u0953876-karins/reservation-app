bfee6d40065a016b5af4179adba542d4c1c6d436
Tomoyuki Kawaguchi <u0953876.karins.soft@icloud.com>
2021-06-23 12:00:46 +0900

    フロントエンドとバックエンドの結合

--- src/app/product/product-listings/product-listings.component.ts
+++ src/app/product/product-listings/product-listings.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { products } from '../../products';
+import { ProductService } from '../shared/product.service';
 
 @Component({
   selector: 'app-product-listings',
@@ -7,13 +7,17 @@ import { products } from '../../products';
   styleUrls: ['./product-listings.component.scss']
 })
 export class ProductListComponent implements OnInit {
-
   products: any
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.products = products
+    const productsObservable = this.productService.getProducts()
+    productsObservable.subscribe(
+      (data) => { 
+        this.products = data
+      },
+      (err) => { console.error('something wrong occurred: ' + err) }
+    )
   }
-
 }
---
+++ src/app/product/shared/product.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<any> {
+    return this.http.get('/api/v1/products')
+  }
+
+  getProductById(productId: string): Observable<any> {
+    return this.http.get('/api/v1/products/' + productId)
+
+  }
+}
